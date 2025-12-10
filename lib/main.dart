928a8a25631117561bb7129f0f61a45761319c25
Ammar Hasan <164255404+ammar23hasan@users.noreply.github.com>
2025-12-10 22:10:28 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'providers/locale_provider.dart';
